ca0cab34b61e9d8995b51b822a1f44bbad026fdd
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-05-23 20:21:58 +0200

    Add files via upload

---
+++ controllers/UtilisateurController.php
@@ -0,0 +1,68 @@
+<?php
+// ============================================================
+// controllers/UtilisateurController.php - ...
+// ============================================================
+
+require_once 'models/Utilisateur.php';
+
+class UtilisateurController {
+
+    private $modele;
+
+    function __construct() {
+        $this->modele = new Utilisateur();
+    }
+
+    public function connexion() {
+        if (isset($_POST['mail'], $_POST['mot_de_passe'])) {
+            $mail = $_POST['mail'];
+            $mot_de_passe = $_POST['mot_de_passe'];
+            $utilisateur = $this->modele->getByMail($mail);
+            if ($utilisateur && password_verify($mot_de_passe, $utilisateur['mot_de_passe'])) {
+                $_SESSION['utilisateur_id'] = $utilisateur['id_utilisateur'];
+                $_SESSION['utilisateur_mail'] = $utilisateur['mail'];
+                header('Location: index.php?page=catalogue');
+                exit();
+            }
+            else {
+                header('Location: index.php?page=connexion&erreur=1');
+                exit();
+            }
+        }
+        else {
+            require 'views/client/connexion.php';
+        }
+    }
+
+    public function inscription() {
+        if (isset($_POST['mail'], $_POST['mot_de_passe'])) {
+            $existant = $this->modele->getByMail($_POST['mail']);
+            if ($existant) {
+                header('Location: index.php?page=inscription&erreur=1');
+                exit();
+            }
+            $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_BCRYPT);
+            $data = [
+                ':nom' => $_POST['nom'],
+                ':prenom' => $_POST['prenom'],
+                ':mail' => $_POST['mail'],
+                ':telephone' => $_POST['telephone'] ?? null,
+                ':adresse' => $_POST['adresse'] ?? null,
+                ':mot_de_passe' => $mot_de_passe
+            ];
+            $this->modele->insert($data);
+            header('Location: index.php?page=connexion');
+            exit();
+        }
+        else {
+            require 'views/client/inscription.php';
+        }
+    }
+
+    public function deconnexion() {
+        session_destroy();
+        header('Location: index.php?page=connexion');
+        exit();
+    }
+}
+?>
