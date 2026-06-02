7b3c0ef8f3ac62ca563c8db17e79d4f9d4ff8531
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-02 14:53:18 +0200

    Expand AdministrateurController with product and category management

--- controllers/AdministrateurController.php
+++ controllers/AdministrateurController.php
@@ -4,13 +4,22 @@
 // ============================================================
 
 require_once 'models/Administrateur.php';
+require_once 'models/Produit.php';
+require_once 'models/CategorieProduit.php';
+require_once 'models/Commande.php';
 
 class AdministrateurController {
 
     private $modele;
+    private $modeleProduit;
+    private $modeleCategorieProduit;
+    private $modeleCommande;
 
     function __construct() {
         $this->modele = new Administrateur();
+        $this->modeleProduit = new Produit();
+        $this->modeleCategorieProduit = new CategorieProduit();
+        $this->modeleCommande = new Commande();
     }
 
     public function connexion() {
@@ -35,6 +44,101 @@ class AdministrateurController {
     public function deconnexion() {
         session_destroy();
         header('Location: index.php?page=login');
+        exit();
+    }
+
+    public function dashboard() {
+        $this->requireAdmin();
+        $produits = $this->modeleProduit->getAll();
+        $commandes = $this->modeleCommande->getAll();
+        $stock_faible = $this->modeleProduit->getStockFaible(5);
+        require 'views/admin/dashboard.php';
+    }
+
+    public function listeProduits() {
+        $this->requireAdmin();
+        $produits = $this->modeleProduit->getAll();
+        $categories = $this->modeleCategorieProduit->getAll();
+        require 'views/admin/produits.php';
+    }
+
+    public function ajouterProduit() {
+        $this->requireAdmin();
+        if (isset($_POST['nom'])) {
+            $data = [
+                ':nom' => $_POST['nom'],
+                ':image' => $_POST['image'],
+                ':prix' => $_POST['prix'],
+                ':quantite' => $_POST['quantite'],
+                ':description' => $_POST['description'],
+                ':id_categorie_produit' => $_POST['id_categorie_produit']
+            ];
+            $this->modeleProduit->insert($data);
+            header('Location: index.php?page=admin_produits');
+            exit();
+        }
+        require 'views/admin/form_produit.php';
+    }
+
+    public function modifierProduit($id) {
+        $this->requireAdmin();
+        $produit = $this->modeleProduit->getById($id);
+        if (isset($_POST['nom'])) {
+            $data = [
+                ':nom' => $_POST['nom'],
+                ':image' => $_POST['image'],
+                ':prix' => $_POST['prix'],
+                ':quantite' => $_POST['quantite'],
+                ':description' => $_POST['description'],
+                ':id_categorie_produit' => $_POST['id_categorie_produit']
+            ];
+            $this->modeleProduit->update($id, $data);
+            header('Location: index.php?page=admin_produits');
+            exit();
+        }
+        require 'views/admin/form_produit.php';
+    }
+
+    public function supprimerProduit($id) {
+        $this->requireAdmin();
+        $this->modeleProduit->delete($id);
+        header('Location: index.php?page=admin_produits');
+        exit();
+    }
+
+    public function listeCategories() {
+        $this->requireAdmin();
+        $categories = $this->modeleCategorieProduit->getAll();
+        require 'views/admin/categories.php';
+    }
+
+    public function ajouterCategorie() {
+        $this->requireAdmin();
+            if (isset($_POST['nom'])) {
+            $data = [':nom' => $_POST['nom']];
+            $this->modeleCategorieProduit->insert($data);
+            header('Location: index.php?page=admin_categories');
+            exit();
+        }
+        require 'views/admin/form_categorie.php';
+    }
+
+    public function modifierCategorie($id) {
+        $this->requireAdmin();
+        $categorie = $this->modeleCategorieProduit->getById($id);
+        if (isset($_POST['nom'])) {
+            $data = [':nom' => $_POST['nom']];
+            $this->modeleCategorieProduit->update($id, $data);
+            header('Location: index.php?page=admin_categories');
+            exit();
+        }
+        require 'views/admin/form_categorie.php';
+    }
+
+    public function supprimerCategorie($id) {
+        $this->requireAdmin();
+        $this->modeleCategorieProduit->delete($id);
+        header('Location: index.php?page=admin_categories');
         exit();
     }
 
