3ac016ad329636a603d709d5f6769dcef24edcca
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-18 16:33:10 +0200

    Refactor inscription.php form structure and labels
    Updated the inscription form layout and added labels for better accessibility.

--- views/client/inscription.php
+++ views/client/inscription.php
@@ -26,17 +26,37 @@
             <?php endif; ?>
 
             <form method="POST" action="index.php?page=inscription">
-                <input type="text" name="nom" placeholder="Nom *" required>
-                <input type="text" name="prenom" placeholder="Prénom *" required>
-                <input type="tel" name="telephone" placeholder="Téléphone">
-                <input type="text" name="adresse" placeholder="Adresse">
-                <input type="email" name="mail" placeholder="E-mail *" required>
-                <input type="password" name="mot_de_passe" placeholder="Mot de passe *" required>
-                <input type="password" name="mot_de_passe_confirm" placeholder="Confirmer le mot de passe" required>
+                <div class="form-group">
+                    <label>Nom <span class="obligatoire">*</span></label>
+                    <input type="text" name="nom" placeholder="Votre nom" required>
+                </div>
+                <div class="form-group">
+                    <label>Prénom <span class="obligatoire">*</span></label>
+                    <input type="text" name="prenom" placeholder="Votre prénom" required>
+                </div>
+                <div class="form-group">
+                    <label>Téléphone</label>
+                    <input type="tel" name="telephone" placeholder="Votre téléphone">
+                </div>
+                <div class="form-group">
+                    <label>Adresse</label>
+                    <input type="text" name="adresse" placeholder="Votre adresse">
+                </div>
+                <div class="form-group">
+                    <label>E-mail <span class="obligatoire">*</span></label>
+                    <input type="email" name="mail" placeholder="Votre e-mail" required>
+                </div>
+                <div class="form-group">
+                    <label>Mot de passe <span class="obligatoire">*</span></label>
+                    <input type="password" name="mot_de_passe" placeholder="Votre mot de passe" required>
+                </div>
+                <div class="form-group">
+                    <label>Confirmer le mot de passe <span class="obligatoire">*</span></label>
+                    <input type="password" name="mot_de_passe_confirm" placeholder="Confirmez votre mot de passe" required>
+                </div>
                 <button type="submit">Créer un compte →</button>
             </form>
-
-            <p class="mention-obligatoire">* Champs obligatoires</p>
+            <p class="mention-obligatoire"><span class="obligatoire">*</span> Champs obligatoires</p>
 
             <a href="index.php?page=connexion">Déjà un compte ?</a>
         </div>
