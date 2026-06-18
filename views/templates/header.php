6c6914980c5d086bdbd3fe747993f5ad6f3d5463
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-18 15:50:27 +0200

    Refactor header.php for consistency and clarity

--- views/templates/header.php
+++ views/templates/header.php
@@ -10,7 +10,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Inkado</title>
-        <link rel="stylesheet" href="eboutique/public/css/style.css">
+        <link rel="stylesheet" href="public/css/style.css">
     </head>
     <body>
         <header>
