1ec4eef113ffae27059cb8a3707bb526266dc468
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-20 12:34:20 +0200

    Add admin dashboard link to footer template

--- views/templates/footer.php
+++ views/templates/footer.php
@@ -8,6 +8,11 @@
             <a href="#">Conditions générales de vente</a>
             <a href="#">Cookies</a>
             <a href="#">Contact</a>
+            <?php if (isset($_SESSION['administrateur_id'])): ?>
+                <a href="index.php?page=dashboard">Dashboard admin</a>
+            <?php else: ?>
+                <a href="index.php?page=login">Espace admin</a>
+            <?php endif; ?>
             <p>© 2026, Inkado</p>
         </footer>
     </body>
