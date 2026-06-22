a81acaa2ed7b204b7e465342aee7c068108b8883
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-22 23:20:06 +0200

    Join Produit table in getByCommande method

--- models/LigneCommande.php
+++ models/LigneCommande.php
@@ -8,7 +8,7 @@ require_once 'models/Model.php';
 class LigneCommande extends Model {
 
     public function getByCommande($id_commande) {
-        $requete = $this->pdo->prepare("SELECT * FROM Ligne_Commande WHERE id_commande = :id_commande");
+        $requete = $this->pdo->prepare("SELECT Ligne_Commande.*, Produit.nom FROM Ligne_Commande JOIN Produit ON Ligne_Commande.id_produit = Produit.id_produit WHERE Ligne_Commande.id_commande = :id_commande");
         $requete->execute([":id_commande" => $id_commande]);
         return $requete->fetchAll();
     }
