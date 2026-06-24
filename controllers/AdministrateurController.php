141bada761211b81d10d67c5ff76ca7c8618b98a
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-24 20:43:16 +0200

    Fix PHP syntax and improve formatting in AdministrateurController

--- controllers/AdministrateurController.php
+++ controllers/AdministrateurController.php
@@ -156,13 +156,13 @@ class AdministrateurController {
     public function updateStatutCommande($id, $statut) {
         $this->requireAdmin();
         $this->modeleCommande->updateStatut($id, $statut);
-        if ($statut === 'Refuse') {
-            $this->modeleCommande->updateStatutLivraison($id, 'Non livre');
+        if ($statut === 'Refusé') {
+            $this->modeleCommande->updateStatutLivraison($id, 'Non livré');
         }
         header('Location: index.php?page=admin_commandes');
         exit();
     }
-    
+
     public function updateStatutLivraison($id, $statut_livraison) {
         $this->requireAdmin();
         $this->modeleCommande->updateStatutLivraison($id, $statut_livraison);
