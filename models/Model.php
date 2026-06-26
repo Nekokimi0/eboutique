1c05cef20db5a58927d1bcf1f7ff1b475654b5bd
Nekokimi0 <75419315+Nekokimi0@users.noreply.github.com>
2026-06-26 14:07:17 +0200

    Refactor comments and formatting in Model.php

--- models/Model.php
+++ models/Model.php
@@ -8,7 +8,7 @@ require_once 'config/database.php';
 
 abstract class Model {
 
-    protected PDO $pdo; // accessible dans toutes les classes filles
+    protected PDO $pdo; // Accessible dans toutes les classes filles
 
     public function __construct() {
         // Récupère la connexion unique définie dans config/database.php
